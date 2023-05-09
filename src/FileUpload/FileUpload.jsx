b351cf6ea2c84225d748354c3f29beb38b86d5d3
riddhijs26 <133056953+riddhijs26@users.noreply.github.com>
2023-05-09 18:48:16 -0400

    Update FileUpload.jsx

--- src/FileUpload/FileUpload.jsx
+++ src/FileUpload/FileUpload.jsx
@@ -44,7 +44,7 @@ const FileUpload = ({ files, setFiles }) => {
 
   const postData = async (data) => {
     try {
-      const res = await axios.post(`${process.env.API_Url}/fileupload`, data);
+      const res = await axios.post(`https://z9dx6v-8000.csb.app/fileupload`, data);
       if (res) {
         data.forEach((file) => (file.isUploading = false));
         setFiles([...files, ...data]);
